2b171817b404865e5ebbf7c0c8fdf63706430f2c
canoijstart <107097281+canoijstart@users.noreply.github.com>
2022-06-08 11:24:02 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,263 @@
+
+<!doctype html>
+<html lang="en-us" prefix="og:http://ogp.me/ns#">
+  <head>
+   <meta charset="utf-8">
+   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="icon" href="assets/images/fav-icon.png" type="image/png" sizes="40x38">
+    <!-- Required meta tags -->
+	<title>https //ij.start.canon | Ij.Start Canon | http //ij.start.canon</title>
+	<meta name="keywords" content="https //ij.start.canon | Ij.Start Canon | http //ij.start.canon">
+     <meta name="description" content="ij.start.canon setup instructions for managing and setting up Canon wireless printer. Visit http //ij.start.canon and enter your model number to install drives">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="assets/css/bootstrap.min.css" type="text/css">
+    <!-- Slick Slider CSS -->
+    <link rel="stylesheet" href="assets/css/slick.css" type="text/css">
+    <!-- Hover Effect CSS -->
+    <link rel="stylesheet" href="assets/css/hover-min.css" type="text/css">
+<!-- fancybox CSS -->
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/fancybox/3.2.5/jquery.fancybox.min.css" />
+    <!-- Fontawesome CSS -->
+    <link rel="stylesheet" href="assets/css/fontawesome-all.min.css" type="text/css">
+    <!--  Theme Css -->
+    <link rel="stylesheet" href="assets/css/style.css" type="text/css">
+	    <!--  Theme Css -->
+    <link rel="stylesheet" href="assets/css/bootstrap-icons.css" type="text/css">
+
+
+  </head>
+
+  <body>
+  <!-- Header Start -->
+  <header class="header sticky-top bg-black">
+<!-- Navabr Start-->
+  <nav class="navbar navbar-expand-lg  ">
+    <div class="container">
+    <a class="navbar-brand" href="/" title="eBookReader"><img src="assets/images/logo.png" class="img-fluid"></a>
+    <!--button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <em class="fas fa-bars"></em>
+      </button-->
+
+    <div class="collapse navbar-collapse" id="navbarNav">
+      <ul class="navbar-nav ms-auto ">
+        <li class="nav-item active">
+          <a class="nav-link" href="/" title="Home">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="/#about-us" title="Know about Router Login">Know about Router Login </a>
+        </li>     
+        
+      </ul>
+      <!--a href="" title="" class="request-a-quote">Download</a-->
+  </div>
+    </div>
+  </nav>
+<!-- Navabr End-->
+</header>
+<!-- Header End -->
+
+<!-- Main Slider start -->
+<section class="p-0 main-slider">
+  <div>
+    <div class="container">
+      <div class="row">
+        <div class="col">
+    <div class="slider-text">
+    <h1 class="text-white font-weight-bold">IJ START PRINTER SETUP</h1>
+        <p class="text-white mb-0"><span class="d-block">Get 24/7 Help for IJ Start Printer Setup</span>
+</div>
+</div>
+</div>
+</div>
+    <img src="assets/images/banner.jpg" alt="IJ START PRINTER SETUP" class="img-fluid">
+  </div>
+</section>
+<!-- Main Slider End -->
+<!-- why the Dataware start -->
+<section id="about-us" class="why-the-dataware pb-4">
+<div class="container">
+  <div class="row">
+  <div class="col-lg-7">
+  <h5 class="text-red text-uppercase">About US</h5>
+    <h2>Get Started with ij.start.canon</h2>
+    <p>Whenever you will buy a new printer, the first thing you have to do is set up the printer with the printer driver’s installation. If you are having a Canon Printer, then you need to install the printer drivers online as per your model. You just need to land on https//ij.start.canon and you can install the desired printer driver. This link will not only help you to have the printer driver installed into your system but also you can get the solutions to your problems associated with the printer as well. So, get to know about everything in just a few steps.</p>
+
+  </div>
+    <div class="col-lg-5"><img src="assets/images/ij-start-canon.jpg" alt="Get Started with ij.start.canon" class="img-fluid hvr-float"></div>
+	
+
+</div>
+  <div class="row">
+  <div class="col-lg-12">
+	<h3>Start Your Printer with ij.start.canon</h3>
+	<p>Canon printers are the best printers for home and office use. Their numerous features and best quality prints make them a good choice. So, if you have made this your first choice and bought it, you need to complete your printer’s setup by signing up in ij.start.canon. It is compatible with both operating systems, i.e. Windows, and macOS. So, you will get to know about the steps for both the operating systems. </p>
+</div>
+</div>
+</div>
+</section>
+
+
+<section class="py-4 bg-gray">
+<div class="container">
+  <div class="row">
+	 <div class="col-lg-12 mt-3"> 
+	<h2>Setup with Windows</h2>
+	<p>If you are having the Windows operating system, then one should follow these steps:</p> 
+</div>
+</div>
+<div class="row mt-3">
+<div class="col-lg-5 "> 
+<img src="assets/images/add-printer-in-windows-10.jpg" alt="Add Printer in Windows 10" class="img-fluid hvr-float">
+</div>
+ <div class="col-lg-7"> 
+<ul class="list-styled">
+<li><strong>Step 1:</strong> First, assemble the printer, and connect it to the system in which you want to do the setup.</li>
+<li><strong>Step 2:</strong>   Now, start your system, and launch your choice web browser like Google Chrome.</li>
+<li><strong>Step 3:</strong> In the URL field, type the official web address, ij.start.canon.</li>
+<li><strong>Step 4:</strong> So, with this, you will land on the official web page of Canon.</li>
+<li><strong>Step 5:</strong>  It will have various options. You have to click on the “Setup” Icon.</li>
+<li><strong>Step 6:</strong>  The page will get open asking you for the series and the product name.</li>
+<li><strong>Step 7:</strong>   After selecting the printer, click over the “Go” button.</li>
+<li><strong>Step 8:</strong>  Now, go to the Start icon, and click on that.</li>
+<li><strong>Step 9:</strong>  The on-screen instructions will appear in the window. Follow them to prepare the printer for you.</li>
+<li><strong>Step 10:</strong>  In the instructions, the download button will appear. Click on the Download button, to start the download.</li>
+<li><strong>Step 11:</strong>  In the Downloads folder, the .exe file format file will appear. Open the file having the same name extension.</li>
+
+</ul>
+
+</div>
+</div>
+<div class="row mt-3">
+<div class="col-lg-12 ">
+<ul class="list-styled">
+<li><strong>Step 12:</strong>  Next, the “User Account Control” will open up on the screen.</li>
+<li><strong>Step 13:</strong>  Click over the Yes button.</li>
+<li><strong>Step 14:</strong>  Now, in the next window, the “Start Setup” button will be there in the next window. Click on that.</li>
+<li><strong>Step 15:</strong>  After the completion of this process, the setup for the Canon Printer will get complete. Now, you can install the driver as required.</li>
+</ul>
+<p>These are the steps for the Windows operating system. In case, you are having the macOS, then you need to follow the steps mentioned in the next section.
+</p>
+</div>
+</div>
+</div>
+</section>
+
+<section class="py-5">
+<div class="container">
+  <div class="row">
+<div class="col-lg-12">
+<h2>Setup with macOS</h2>  
+    <p>In case, you are having the system from the Apple manufacturer, then you need to follow the steps given below:</p>
+	
+
+	
+</div>
+</div>	
+	  <div class="row">
+<div class="col-lg-7">
+<ul class="list-styled">
+<li><strong>Step 1:</strong>: First, assemble the system, and connect it to your system.</li>
+<li><strong>Step 2:</strong>  Start your system, and then open Safari as the internet browser of your system.</li>
+<li><strong>Step 3:</strong> In the URL field, type the official web address, ij.start.canon.</li>
+<li><strong>Step 4:</strong> So, with this, you will land on the official web page of Canon.</li>
+<li><strong>Step 5:</strong> It will have various options. You have to click on the “Setup” Icon.</li>
+<li><strong>Step 6:</strong> The page will get open asking you for the series and the product name.</li>
+<li><strong>Step 7:</strong> After selecting the printer, click over the “Go” button.</li>
+<li><strong>Step 8:</strong> Now, go to the Start icon, and click on that.</li>
+<li><strong>Step 9:</strong> The on-screen instructions will appear in the window. Follow them to prepare the printer for you.</li>
+<li><strong>Step 10:</strong> In the instructions, the download button will appear. Click on the Download button, to start the download.</li>
+<li><strong>Step 11:</strong> In the Downloads folder, the .dmg file format file will appear. Open the file having the same name extension.</li>
+<li><strong>Step 12:</strong> Next, the “User Account Control” will open up on the screen.</li>
+<li><strong>Step 13:</strong> Click over the Yes button</li>
+<li><strong>Step 14:</strong> Now, in the next window, the “Start Setup” button will be there. Click on that.</li>
+<li><strong>Step 15:</strong> With this, you will complete the setup of the Canon Printer in macOS.</li>
+</ul>
+
+</div>
+<div class="col-lg-5">
+<img src="assets/images/ij-start-printer-setup-in-mac	.jpg" alt="Add Printer in Mac" class="img-fluid hvr-float">
+</div>
+
+	</div>
+
+<div class="row mt-3">
+<div class="col-lg-12">
+<p>Therefore, with this setup guide, you can successfully download the setup for Canon Printer and can use it according to your use.
+</p>
+<h4>Get Better Quality Prints</h4>
+<p>When one buys the printer or scanner, the very first thing they ask the dealer is about the print quality as it matters the most. For some, printers play an important role in their daily life. So, the Canon printers are best for them. The only thing they must take care of is the download of printer drivers with https//ij.start.canon, and they are all set to get better quality prints at their own place.</p>
+
+</div>
+</div>
+
+</div>
+</section>
+
+
+
+
+
+
+
+<!-- Footer Start-->
+<footer class="footer">
+  <div class="top-footer">
+<div class="container"> 
+<div class="row">
+   <div class="col-md-12 mt-3">
+  <strong>Disclaimer</strong> <p>https-ij-start.com is an independent online technical support service provider company for third party products, brands and services. The brand names, images, trademarks, products and services of third parties mentioned on this website are only for referential purposes only and https-ij-start.com has no affiliation with any of these third-party companies unless such relationship is expressed specifically. The services we offer is also available on the website of the brand owners .</p>
+  </div>
+</div>
+</div>
+
+
+
+  </div>
+
+
+
+  <div class="bottom-footer">
+  <div class="container">
+    <div class="row">
+      <div class="col-md-5">
+	  <ul class="list-unstyled d-inline">
+        <li>
+          <a href="disclaimer" title="Press">Disclaimer</a>
+        </li>
+      </ul>
+      
+      </div>
+	   <div class="col-md-6">
+     <p class="mb-0"> Copyright © 2022 |<a href="/" title="https-ij-start.com"> https-ij-start.com</a> | All Rights Reserved  </p>
+      </div>
+    </div>
+  </div>
+
+
+
+
+
+    </div>
+</footer>
+<!-- Footer close -->
+
+<!-- Go to top -->
+<button id="gototop" title="Go to top" class="gototop"><em class="fas fa-angle-up"></em></button>
+
+
+<!-- JavaScript -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.4/jquery.min.js"></script>
+<!-- jQuery first, then Popper.js, then Bootstrap JS -->
+<script src="assets/js/bootstrap.bundle.min.js" type="text/javascript"></script>
+<script src="assets/js/slick.min.js" type="text/javascript"></script>
+
+<!--Custom Function JS -->
+<script src="assets/js/main.js"></script>
+
+       
+
+
+  </body>
+</html>
+
